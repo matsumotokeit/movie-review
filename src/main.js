403e60c00445e8613f54f6b088f66ccc5c11f324
Matsumoto Keito <matsumotokeito@matsumotonoMacBook-Air.local>
2025-05-20 07:56:45 +0900

    initial commit（Vue CLI + Vuetify + Axios 環境構築完了）

--- src/main.js
+++ src/main.js
@@ -2,5 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
 
-createApp(App).use(store).use(router).mount('#app')
+const vuetify = createVuetify()
+
+const app = createApp(App)
+app.use(router)
+app.use(store)
+app.use(vuetify)
+app.mount('#app')
